9e8a20fa00b4728f874fc7fd511fda07fc7722e9
José Carlos Gutiérrez Rojas <feucarlos@gmail.com>
2024-12-09 18:25:25 -0600

    básico - tema 01 terminado

--- básico/tema01/ejercicio_6.py
+++ básico/tema01/ejercicio_6.py
@@ -1,5 +1,25 @@
+# Comprueba si la cadena es un número flotante válido
+def is_valid_float(element: str) -> bool:
+    try: 
+        float(element)
+        return True
+    except ValueError:
+        return False
+
+
+# Se pide ingresar un número
 numero = input("Dame un número decimal: ")
+numero = numero.strip() #se borran espacios en blanco
 
-entero = round(numero)
+if(is_valid_float(numero)):
+    numero = int(float(numero)) # trunca y deja la parte entera
+elif(numero.isdecimal()):
+    numero = int(numero)
 
-print(entero)
+if (type(numero) == int):
+    print("cadena: ", numero)
+    print("el dato  es ", type(numero))
+    numero = str(numero)
+    print("ahora es ", type(numero))
+else:
+    print("Dato no válido")
